16986d491e9e66d22b086d53647ab6cb8ddd455c
Pedro Henrique Maia Alves <160970721+PedroMaiaAlves@users.noreply.github.com>
2024-03-18 18:47:38 -0300

    Add files via upload

---
+++ Exercise1-1103/src/Exercise1-1103.c
@@ -0,0 +1,36 @@
+/*
+ ============================================================================
+ Name        : exercise1.c
+ Author      : Pedro Henrique Maia Alves
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int contpar = 0, contimpar = 0, num = -1;
+
+	while (num != 0) {
+		printf("Entre com um número; para encerrar, digite 0: ");
+		fflush(stdout);
+		scanf("%d", &num);
+
+		if (num != 0) {
+			if (num % 2 == 0) {
+				contpar = contpar + 1;
+			} else {
+				contimpar = contimpar + 1;
+			}
+
+		}
+	}
+
+	printf("A quantidade de números pares é: %d\n", contpar);
+	printf("A quantidade de números impares é: %d\n", contimpar);
+
+	return 0;
+}
